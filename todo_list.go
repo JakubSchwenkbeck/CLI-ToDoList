87b396f194f45cfba040bfb2d30ce1e54a4ec758
Jakub Schwenkbeck <Jakub@eSchwenkbeck>
2024-09-01 21:57:13 +0200

    implementation erros occured

--- todo_list.go
+++ todo_list.go
@@ -1,47 +1,15 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
-var filePath = "todo.txt"
-
-// Function to add a task
-func addTask(task string) error {
-	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	if _, err := file.WriteString(task + "\n"); err != nil {
-		return err
-	}
-	return nil
-}
-
-// Function to list all tasks
-func listTasks() error {
-	file, err := os.Open(filePath)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-	}
-	return scanner.Err()
-}
-
-// Main function
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: todo_list <command> [task]")
+		fmt.Println("Usage: todo <command> [arguments...]")
 		return
 	}
 
@@ -49,7 +17,7 @@ func main() {
 	switch command {
 	case "add":
 		if len(os.Args) < 3 {
-			fmt.Println("Usage: todo_list add <task>")
+			fmt.Println("Usage: todo add <task>")
 			return
 		}
 		task := strings.Join(os.Args[2:], " ")
@@ -59,11 +27,52 @@ func main() {
 			fmt.Println("Task added:", task)
 		}
 	case "list":
-		if err := listTasks(); err != nil {
-			fmt.Println("Error listing tasks:", err)
+		if len(os.Args) > 2 {
+			switch os.Args[2] {
+			case "all", "pending", "completed":
+				if err := listTasks(os.Args[2]); err != nil {
+					fmt.Println("Error listing tasks:", err)
+				}
+			default:
+				fmt.Println("Usage: todo list <all|pending|completed>")
+			}
+		} else {
+			if err := listTasks("all"); err != nil {
+				fmt.Println("Error listing tasks:", err)
+			}
+		}
+	case "complete":
+		if len(os.Args) < 3 {
+			fmt.Println("Usage: todo complete <task_index>")
+			return
+		}
+		index, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			fmt.Println("Invalid task index:", err)
+			return
+		}
+		if err := markTaskCompleted(index - 1); err != nil {
+			fmt.Println("Error marking task completed:", err)
+		} else {
+			fmt.Println("Task marked as completed")
+		}
+	case "remove":
+		if len(os.Args) < 3 {
+			fmt.Println("Usage: todo remove <task_index>")
+			return
+		}
+		index, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			fmt.Println("Invalid task index:", err)
+			return
+		}
+		if err := removeTask(index - 1); err != nil {
+			fmt.Println("Error removing task:", err)
+		} else {
+			fmt.Println("Task removed")
 		}
 	default:
-		fmt.Println("Usage: todo_list <command> [task]")
-		fmt.Println("Commands: add, list")
+		fmt.Println("Usage: todo <command> [arguments...]")
+		fmt.Println("Commands: add, list, complete, remove")
 	}
 }
